42033740e8424c756106ef42a2f56b3a47095f6b
Try2Teachme <regboxformail@gmail.com>
2023-03-15 21:24:11 -0400

    Selenium Grid with Jenkins Oscar

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "34.205.65.97";
+                        String gridAddress = "100.25.188.15";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
